5b234e34542b53a5f847b1f993d639239ff09107
krissuNew <krzysztofrusak91@gmail.com>
2018-05-20 23:54:54 +0200

    Można wpisywać liczby ręcznie lub klikać przyciski(ale tylko liczby pojedenczo)

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,11 +1,8 @@
 package controllers;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-
-import java.awt.event.ActionEvent;
 
 public class MainController {
 
@@ -43,17 +40,18 @@ public class MainController {
     private Button buttonDivide;
     @FXML
     private Button buttonClean;
-    @FXML
-    private Button buttonDelete;
-    @FXML
-    private Button buttonDot;
 
 
+    private int valueOfNumberOne;
+    private int valueOfNumberTwo;
 
-    private int liczba1;
-    private int liczba2;
+    private boolean addNumbers;
+    private boolean substractNumbers;
+    private boolean multiplyNumbers;
+    private boolean divideNumbers;
 
-    public void initialize(){
+
+    public void initialize() {
 
         numberOne.setOnAction(event -> textfieldResult.setText(String.valueOf(1)));
         numberTwo.setOnAction(event -> textfieldResult.setText(String.valueOf(2)));
@@ -67,24 +65,70 @@ public class MainController {
         numberZero.setOnAction(event -> textfieldResult.setText(String.valueOf(0)));
 
 
+//Przycisk dodawania
+            buttonAdd.setOnAction(event -> {
+                getNumberOneAndCleanTextfieldToEnterNumberTwo();
+                addNumbers = true;
+            });
 
-        buttonAdd.setOnAction(event -> {
-            liczba1 = Integer.valueOf(textfieldResult.getText());
-            textfieldResult.clear();
-        });
+//Przycisk odejmowania
+            buttonSubstract.setOnAction(event -> {
+                getNumberOneAndCleanTextfieldToEnterNumberTwo();
+                substractNumbers = true;
+            });
+
+// Przycisk mnożenia
+            buttonMultiply.setOnAction(event -> {
+                getNumberOneAndCleanTextfieldToEnterNumberTwo();
+                multiplyNumbers = true;
+            });
+
+//Przycisk dzielenia
+            buttonDivide.setOnAction(event -> {
+                getNumberOneAndCleanTextfieldToEnterNumberTwo();
+                divideNumbers = true;
+            });
 
 
-        buttonEqual.setOnAction(event -> {
-            liczba2 = Integer.valueOf(textfieldResult.getText());
+            buttonEqual.setOnAction(event -> {
+                valueOfNumberTwo = Integer.valueOf(textfieldResult.getText());
+                textfieldResult.setText(showResult(valueOfNumberOne, valueOfNumberTwo));
+                resetButtons();
+            });
 
-            textfieldResult.setText(showResult(liczba1, liczba2));
-        });
+            buttonClean.setOnAction(event -> {
+                textfieldResult.clear();
+                resetButtons();
+            });
 
+        }
+
+    private void resetButtons() {
+        addNumbers = false;
+        substractNumbers = false;
+        multiplyNumbers = false;
+        divideNumbers = false;
+    }
+
+
+    private void getNumberOneAndCleanTextfieldToEnterNumberTwo() {
+        valueOfNumberOne = Integer.valueOf(textfieldResult.getText());
+        textfieldResult.clear();
     }
 
     private String showResult(int liczba1, int liczba2) {
+        if (addNumbers) {
+            return liczba1 + " + " + liczba2 + " = " + String.valueOf(liczba1 + liczba2);
+        } else if (substractNumbers){
+            return liczba1 + " - " + liczba2 + " = " +  String.valueOf(liczba1 - liczba2);
+        } else if (multiplyNumbers) {
+            return liczba1 + " * " + liczba2 + " = " + String.valueOf(liczba1 * liczba2);
+        }   else if (divideNumbers) {
+            double result = (double)liczba1 / liczba2;
+            return liczba1 + " / " + liczba2 + " = " +  String.valueOf(result);
+        } else
+            return "Nie umiem przeliczyć";
 
-        return null;
     }
 }
 
